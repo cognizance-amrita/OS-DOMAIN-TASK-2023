e314caba84cb6aa805a607f3a6a8385ad9234e38
Umme Rooman <99654067+ummerooman@users.noreply.github.com>
2023-08-17 17:45:35 +0530

    Update bughunt.py

--- TASK-2/bughunt.py
+++ TASK-2/bughunt.py
@@ -1,37 +1,105 @@
-def fibonacci_sequence(n):
-    fibonacci = []
-    a, b = 0, 1
-    for i in range(n):
-        fibonacci.append(a)
-        a = b
-        b = a + b
-    return fibonacci
+def greet(name):
+    print("Hello, " + name)  
 
-def is_prime(num):
-    if num < 2:
+def calculate_sum(nums):
+    total = 0
+    for num in nmus:  
+        total += num
+    return toatl 
+
+def is_prime(n):
+    if n < 2:
         return False
-    for i in range(2, num):
-        if num % i = 0:
-            return False
-    return True
+    for i in range(2, sqrt(n) + 1):  
+        if n % i == 0:
+            return True  
 
-def analyze_numbers(numbers):
-    even_sum = 0
-    odd_sum = 0
-    prime_count = 0
-    for num in numbers:
-        if num % 2 = 0:
-            even_sum += num
-        else:
-            odd_sum += num
+def print_prime_numbers(limit):
+    primes = []
+    num = 2
+    while len(prims) < limit:  
         if is_prime(num):
-            prime_count += 1
-    return even_sum, odd_sum, prime_count
+            primes.append(num)
+        num += 1
+    print("First", limit, "prime numbers are:", primes)
 
-numbers = [2, 5, 8, 13, 18, 21, 25, 29, 32, 37]
-even_sum, odd_sum, prime_count = analyze_numbers(numbers)
-fibonacci_sequence = fibonacci_sequence(10)
-print("Even Sum:", even_sum)
-print("Odd Sum:", odd_sum)
-print("Number of Primes:", prime_count)
-print("Fibonacci Sequence:", fibonacci_sequence)
+name = input("Enter your name: ")
+gret(name)  
+
+numbers = [1, 2, 3, 4, 5]
+total = calculate_sum(number)  
+
+print("Sum of numbers is: " + total)  
+
+print_prime_number(5)  
+
+for i in range(len(numbers)):
+    print("Number at index", i + 1, "is:", numbers[i])  
+
+x = 10
+if x > 5:
+    y = 20
+print(y)  
+
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x - 1)
+print("Factorial of 5 is:", factoria(5))  
+
+for i in range(1, 11):
+    print(i)
+    if i = 5:  
+        print("Halfway there!")
+
+fruits = ['apple', 'banana', 'cherry']
+print("My favorite fruit is:", fruits[3])  
+
+def power(base, exp):
+    result = base ** expo  
+    return result
+
+print("2 to the power of 3 is:", power(2, 3))
+
+value = 100
+if value > 75:
+    print("High value!")
+elif value > 50:
+    print("Medium value!")
+else:
+    print("Low value!")
+
+text = "Hello, world!"
+print(text[7:])  
+
+colors = {'red': 1, 'green': 2, 'blue': 3}
+print("Value for 'yellow' is:", colors.get('yellow'))  
+
+numerator = 10
+denominator = 0
+result = numeratr / denominator  
+
+print("Result:", result)
+
+age = input("Enter your age: ")
+if age < 18:  
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+
+def print_square(num):
+    print("Square of", num, "is:", num * num)
+
+print_squre(5)  
+
+def cube(x):
+    return x ** 3
+
+print("Cube of 3 is:", cuube(3))  
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbes:  
+    print(number)
+
+print("End of program")
